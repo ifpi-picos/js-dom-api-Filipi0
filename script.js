5245eaec89b4dfefd77c01c98e682fc39f869e07
Filipi Dantas <127785380+Filipi0@users.noreply.github.com>
2024-04-04 17:39:38 -0300

    Adiocnando api

--- script.js
+++ script.js
@@ -6,9 +6,25 @@ document.addEventListener('DOMContentLoaded', function () {
         tasks = JSON.parse(storedTasks);
         updateTaskList();
     }
+
+    gapi.load('client', initGoogleCalendarAPI);
 });
 
-function addTask() {
+async function initGoogleCalendarAPI() {
+    try {
+        await gapi.client.init({
+            apiKey: 'AIzaSyBwInZpKqJK-ma6CpIZi5quNFr9JkaEmMI',
+            clientId: '310399738445-pg8kdpkiqui89vsq43g7ak6uq3n1hdf0.apps.googleusercontent.com',
+            discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
+            scope: "https://www.googleapis.com/auth/calendar.events"
+        });
+        console.log('API do Google Calendar inicializada com sucesso!');
+    } catch (error) {
+        console.error('Erro ao inicializar a API do Google Calendar:', error);
+    }
+}
+
+async function addTask() {
     const title = document.getElementById('task-title').value;
     const description = document.getElementById('task-description').value;
     const task = { title, description, status: 'pendente' };
@@ -17,6 +33,30 @@ function addTask() {
     document.getElementById('addTaskForm').reset();
 
     localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    // Criar evento no Google Calendar
+    try {
+        const event = {
+            'summary': title,
+            'description': description,
+            'start': {
+                'dateTime': new Date().toISOString(),
+                'timeZone': 'America/Sao_Paulo'
+            },
+            'end': {
+                'dateTime': new Date().toISOString(),
+                'timeZone': 'America/Sao_Paulo'
+            }
+        };
+
+        const response = await gapi.client.calendar.events.insert({
+            'calendarId': 'primary',
+            'resource': event
+        });
+        console.log('Evento criado no Google Calendar:', response.result);
+    } catch (error) {
+        console.error('Erro ao criar evento no Google Calendar:', error);
+    }
 }
 
 function markAsDone() {
